51ae6275499b87a704050dbbfa461057928a2e81
Maxim <max@injectiveprotocol.com>
2019-12-26 15:55:22 +0530

    Update oz configs to support Ropsten and Matic.

--- networks.js
+++ networks.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const infuraProjectId = process.env.INFURA_PROJECT_ID;
+const infuraRopstenEndpoint = "https://ropsten.infura.io/v3/" + infuraProjectId;
 const infuraRinkebyEndpoint = "https://rinkeby.infura.io/v3/" + infuraProjectId;
+const alchemyRopstenEndpoint = process.env.ALCHEMY_ROPSTEN_ENDPOINT;
 const alchemyRinkebyEndpoint = process.env.ALCHEMY_RINKEBY_ENDPOINT;
 const alchemyKovanEndpoint = process.env.ALCHEMY_KOVAN_ENDPOINT;
+const maticTestnetEndpoint = process.env.MATIC_TESTNET_ENDPOINT;
 
 module.exports = {
   networks: {
@@ -16,6 +19,10 @@ module.exports = {
       gasPrice: 5e9,
       networkId: '*',
     },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, alchemyRopstenEndpoint ? alchemyRopstenEndpoint : infuraRopstenEndpoint),
+      networkId: 3,       // Ropsten's id
+    },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, alchemyRinkebyEndpoint ? alchemyRinkebyEndpoint : infuraRinkebyEndpoint),
       networkId: 4,       // Rinkeby's id
@@ -23,6 +30,10 @@ module.exports = {
     kovan: {
       provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, alchemyKovanEndpoint),
       networkId: 42,       // Kovan's id
+    },
+    matic: {
+      provider: () => new HDWalletProvider(process.env.DEV_MNEMONIC, maticTestnetEndpoint),
+      networkId: 15001,    // Matic Tesnet id
     },
   },
 };
